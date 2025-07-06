26d5a4bf8c11059b588b717ccec4b46adab852d3
mukesh <msmukeshkumarsharma95@gmail.com>
2025-07-06 09:58:33 +0530

    created a route for all contacts

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 const app = express();
 
+app.get('/api/contacts', (req, res) => {
+    res.status(200).json({
 
-app.listen(PORT, () => {
-    console.log(`sever is running on port http://localhost:${PORT}`);
+        "message": "server is running"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`sever is running on port http://localhost:${port}`);
     
 })
 
