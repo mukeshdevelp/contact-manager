dc11b527eb82c0e9dcdb9160a49877225b643f2b
mukesh <msmukeshkumarsharma95@gmail.com>
2025-07-06 09:49:15 +0530

    server is running fine

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 5000;
+const app = express();
+
+
+app.listen(PORT, () => {
+    console.log(`sever is running on port http://localhost:${PORT}`);
+    
+})
+
+
+
