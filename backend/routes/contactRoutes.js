096d20b0c48ec70252b5871f125d3248461832d8
mukesh <msmukeshkumarsharma95@gmail.com>
2025-07-06 10:52:47 +0530

    all routes are currently working

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -7,8 +7,34 @@ const router = express.Router();
 router.route('/').get((req, res) => {
     res.status(200).json({
 
-        "message": "server is running"
+        "message": "get all contacts"
     });
 });
+
+// making extra routes
+router.route('/').post((req, res) =>{
+    res.status(200).json({
+        "message": "contact created"
+    })
+});
+
+//gettting individual contact based on id
+router.route('/:id').get((req,res) => {
+    res.status(200).json({
+        "message": `get contact for ${req.params.id}`
+    })
+})
+router.route('/:id').put((req,res) => {
+    res.status(200).json({
+        "message": `update contact for ${req.params.id}`
+    })
+})
+
+router.route('/:id').delete((req,res) => {
+    res.status(200).json({
+        "message": `contact deleted for ${req.params.id}`
+    })
+})
+
 
 module.exports = router;
