a1738ff3e0a421cdd93b558cb48fd6c736160f94
mukesh <msmukeshkumarsharma95@gmail.com>
2025-07-06 12:32:26 +0530

    added middleware and use errorhandler middleware

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,55 @@
+const {constants} = require('./../constants')
+const errorhandler = (err, req,res) => {
+
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    switch(statusCode){
+        case constants.VALIDATION_ERROR:
+            res.json({
+            title: 'Validation Error',
+            message : err.message,
+            stackTrace : err.stackTrace
+        })
+        break;
+        case constants.NOT_FOUND:
+            // creating response.json
+        res.json({
+            title: 'Not Found',
+            message : err.message,
+            stackTrace : err.stackTrace
+        })
+        break;
+        case constants.UNAUTHORIZED:
+            // creating response.json
+        res.json({
+            title: 'Unauthorized access',
+            message : err.message,
+            stackTrace : err.stackTrace
+        })
+        break;
+        case constants.FORBIDDEN:
+            // creating response.json
+        res.json({
+            title: 'Access Forbidden',
+            message : err.message,
+            stackTrace : err.stackTrace
+        })
+        break;
+        case constants.SERVER_ERROR:
+        // creating response.json
+        res.json({
+            title: 'Server Error',
+            message : err.message,
+            stackTrace : err.stackTrace
+        })
+        break;
+        default:
+            console.log('No Error, All Good!');
+            
+
+    }
+    
+
+    
+}
+
+module.exports = errorhandler;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const errorHandler = require('./middleware/errorHandler')
 const port = process.env.PORT || 5000;
 const app = express();
 // adding a middleware for data parsing
 app.use(express.json());
 app.use('/api/contacts', require('./routes/contactRoutes'));
-
+// error handler middleware
+app.use(errorHandler)
 app.listen(port, () => {
     console.log(`sever is running on port http://localhost:${port}`);
     
