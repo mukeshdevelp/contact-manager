5581d8fddcac1b4ad73d97b4e4061ce0fb43cf8c
mukesh <msmukeshksharma95@gmail.com>
2025-07-07 13:21:43 +0530

    routes created

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.post('/register', (req,res) => {
+    res.status(200).json({"message": "register page"})
+});
+
+router.post('/login', (req,res) => {
+    res.status(200).json({"message": "login page"})
+});
+router.post('/current', (req,res) => {
+    res.status(200).json({"message": "current user page"})
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const app = express();
 // adding a middleware for data parsing
 app.use(express.json());
 app.use('/api/contacts', require('./routes/contactRoutes'));
+// adding user routes
+app.use('/api/users', require('./routes/userRoutes'));
 // error handler middleware
 app.use(errorHandler)
 app.listen(port, () => {
