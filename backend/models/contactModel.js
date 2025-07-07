1c300b198ecb2e23318a7488d4d5e15612885805
mukesh <msmukeshksharma95@gmail.com>
2025-07-07 10:51:13 +0530

    added the mongoose schema

---
+++ backend/models/contactModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoogse');
+
+const contactSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Please add conatct name"]
+    },
+    email:{
+        type: String,
+        required: [true, "Please add conatct email"]
+    },
+     email:{
+        type: String,
+        required: [true, "Please add conatct phone number"]
+    },
+
+}, 
+{
+    // timestamps
+    timestamps: true,
+}
+);
+
+module.exports = mongoose.model("Contact", contactSchema);
